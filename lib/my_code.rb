f0651ea3c6314afd301c1987da7cd9879acbc624
Edgar Rivera <edgar.a.rivera08@gmail.com>
2020-02-12 19:10:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,5 +11,16 @@ def map(array)
 end 
 
 def reduce(array, start=nil)
-  
+  if start 
+    sum = start
+    i = 0 
+  else 
+    sum = array[0]
+    i = 1 
+  end 
+  while i < array.length do 
+    sum = yield(sum, array[i])
+    i += 1 
+  end 
+  sum 
 end
